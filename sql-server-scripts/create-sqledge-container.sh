2088565a6d9fa402afdb6612500605d809792b06
Davide De Pretto <42669027+dadepretto@users.noreply.github.com>
2021-10-17 20:41:47 +0200

    Fix host path

--- sql-server-scripts/create-sqledge-container.sh
+++ sql-server-scripts/create-sqledge-container.sh
@@ -3,10 +3,10 @@ docker run \
     --name sqledge \
     --env ACCEPT_EULA=Y \
     --env MSSQL_SA_PASSWORD=<YourSqlPasswordHere> \
-    --volume /Users/<YourSystemUsername>/docker/mssql/staging:/var/opt/mssql/staging \
-    --volume /Users/<YourSystemUsername>/docker/mssql/data:/var/opt/mssql/data \
-    --volume /Users/<YourSystemUsername>/docker/mssql/log:/var/opt/mssql/log \
-    --volume /Users/<YourSystemUsername>/docker/mssql/secrets:/var/opt/mssql/secrets \
+    --volume /Users/<YourSystemUsername>/docker/sqledge/staging:/var/opt/mssql/staging \
+    --volume /Users/<YourSystemUsername>/docker/sqledge/data:/var/opt/mssql/data \
+    --volume /Users/<YourSystemUsername>/docker/sqledge/log:/var/opt/mssql/log \
+    --volume /Users/<YourSystemUsername>/docker/sqledge/secrets:/var/opt/mssql/secrets \
     --publish 1433:1433 \
     --restart always \
     mcr.microsoft.com/azure-sql-edge
