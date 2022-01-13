45ee259b14d1d2e7de071c984b8cf27def3dfd16
Davide De Pretto <davide.depretto@icloud.com>
2022-01-13 11:14:45 +0100

    Add script to configure ssh

---
+++ shell-scripts/config-ssh.sh
@@ -0,0 +1,21 @@
+comment=${1-$(hostname)}
+filename=${2-github}
+
+echo "-> Generate key"
+ssh-keygen -t rsa -b 4096 -a 128 -f ~/.ssh/${filename} -q -N "" -C $comment
+
+echo "-> Running ssh agent"
+eval "$(ssh-agent -s)" >> /dev/null
+
+echo "-> Adding ssh configuration"
+echo "Host *
+  AddKeysToAgent yes
+  UseKeychain yes
+  IdentityFile ~/.ssh/${filename}
+" >> ~/.ssh/config
+
+echo "-> Adding key to ssh agent"
+ssh-add -q --apple-use-keychain ~/.ssh/${filename}
+
+echo "-> Copying public key to your clipboard. Use CMD+V (paste) to paste wherever you need!"
+cat ~/.ssh/${filename}.pub | pbcopy
