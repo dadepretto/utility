1f4330a240b8a05fd638f7220cdadd639b84dc37
Davide De Pretto <davide.depretto@icloud.com>
2022-01-13 11:15:01 +0100

    Review script to customize zsh on macOS

---
+++ shell-scripts/customize-shell.sh
@@ -0,0 +1,22 @@
+filename="~/.zshrc"
+
+if ! /usr/bin/grep -q 'prompt=' "$filename"; then
+    echo "-> Setting prompt style"
+    echo 'prompt="%1~ %# "' >> $filename
+else
+    echo "-> Skipping prompt style"
+fi
+
+if ! /usr/bin/grep -q 'alias l=' "$filename"; then
+    echo "-> Setting alias 'l' for ls"
+    echo 'alias l="ls -1GlS"' >> $filename
+else
+    echo "-> Skipping alias 'l' for ls"
+fi
+
+if ! /usr/bin/grep -q 'alias c=' "$filename"; then
+    echo "-> Setting alias 'c' for clear"
+    echo 'alias c="clear"' >> $filename
+else
+    echo "-> Skipping alias 'c' for clear"
+fi
--- shell-scripts/my-shell-customizations.sh
+++
@@ -1,8 +0,0 @@
-# Style prompt to be "folder %"
-echo 'prompt="%1~ %# "' >> ~/.zshrc
-
-# Add alias for "l" to be a formatted "ls"
-echo 'alias l="ls -1GlS"' >> ~/.zshrc
-
-# Add alias for "c" to be "clear"
-echo 'alias c="clear"' >> ~/.zshrc
