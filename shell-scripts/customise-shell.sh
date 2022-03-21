109a51f7b572b25cbd69da7d86b44bc2b6176f64
Davide De Pretto <davide.depretto@icloud.com>
2022-03-21 23:45:43 +0100

    Organize zsh configuration

--- shell-scripts/customise-shell.sh
+++ shell-scripts/customise-shell.sh
@@ -1,29 +1,27 @@
-filename="$HOME/.zshrc"
-
-if ! /usr/bin/grep -q 'prompt=' "$filename"; then
+if ! /usr/bin/grep -q 'prompt=' "$HOME/.zshrc"; then
     echo "-> Setting prompt style"
-    echo 'prompt="%1~ %# "' >> $filename
+    echo 'prompt="%1~ %# "' >> $HOME/.zshrc
 else
     echo "-> Skipping prompt style"
 fi
 
-if ! /usr/bin/grep -q 'alias l=' "$filename"; then
+if ! /usr/bin/grep -q 'alias l=' "$HOME/.zshrc"; then
     echo "-> Setting alias 'l' for ls"
-    echo 'alias l="ls -1GlS"' >> $filename
+    echo 'alias l="ls -1GlS"' >> $HOME/.zshrc
 else
     echo "-> Skipping alias 'l' for ls"
 fi
 
-if ! /usr/bin/grep -q 'alias c=' "$filename"; then
+if ! /usr/bin/grep -q 'alias c=' "$HOME/.zshrc"; then
     echo "-> Setting alias 'c' for clear"
-    echo 'alias c="clear"' >> $filename
+    echo 'alias c="clear"' >> $HOME/.zshrc
 else
     echo "-> Skipping alias 'c' for clear"
 fi
 
-if ! /usr/bin/grep -q 'alias docker=' "$filename"; then
+if ! /usr/bin/grep -q 'alias docker=' "$HOME/.zshrc"; then
     echo "-> Setting alias 'docker' for podman"
-    echo 'alias docker="podman"' >> $filename
+    echo 'alias docker="podman"' >> $HOME/.zshrc
 else
     echo "-> Skipping alias 'c' for clear"
 fi
--- shell-scripts/install-ruby.sh
+++ shell-scripts/install-ruby.sh
@@ -2,9 +2,8 @@ brew install rbenv ruby-build -q
 rbenv install 3.0.0 -s
 rbenv global 3.0.0
 
-filename="/Users/$(whoami)/.zshrc"
-if ! /usr/bin/grep -q 'eval "$(rbenv init - zsh)"' "$filename"; then
-    echo 'eval "$(rbenv init - zsh)"' >> ~/.zshrc
+if ! /usr/bin/grep -q 'eval "$(rbenv init - zsh)"' "$HOME/.zprofile"; then
+    echo 'eval "$(rbenv init - zsh)"' >> $HOME/.zprofile
     eval "$(rbenv init - zsh)"
     exec zsh
 fi
