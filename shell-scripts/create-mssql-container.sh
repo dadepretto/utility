928d16e15995b2e89cc32502540e75a2b41f689b
Davide De Pretto <davide.depretto@icloud.com>
2022-03-21 23:29:13 +0100

    Add paramertrization

--- shell-scripts/create-mssql-container.sh
+++ shell-scripts/create-mssql-container.sh
@@ -1,12 +1,15 @@
+initial_password=${1-P@55w0rd}
+container_name=${2-sqledge}
+
 docker run \
     --detach \
-    --name mssql \
+    --name $container_name \
     --env ACCEPT_EULA=Y \
-    --env MSSQL_SA_PASSWORD=<YourSqlPasswordHere> \
-    --volume /Users/<YourSystemUsername>/docker/mssql/staging:/var/opt/mssql/staging \
-    --volume /Users/<YourSystemUsername>/docker/mssql/data:/var/opt/mssql/data \
-    --volume /Users/<YourSystemUsername>/docker/mssql/log:/var/opt/mssql/log \
-    --volume /Users/<YourSystemUsername>/docker/mssql/secrets:/var/opt/mssql/secrets \
+    --env MSSQL_SA_PASSWORD=$initial_password \
+    --volume $HOME/Containers/mssql/staging:/var/opt/mssql/staging \
+    --volume $HOME/Containers/mssql/data:/var/opt/mssql/data \
+    --volume $HOME/Containers/mssql/log:/var/opt/mssql/log \
+    --volume $HOME/Containers/mssql/secrets:/var/opt/mssql/secrets \
     --publish 1433:1433 \
     --restart always \
     mcr.microsoft.com/mssql/server:2019-latest
--- shell-scripts/create-sqledge-container.sh
+++ shell-scripts/create-sqledge-container.sh
@@ -1,12 +1,15 @@
+initial_password=${1-P@55w0rd}
+container_name=${2-sqledge}
+
 docker run \
     --detach \
-    --name sqledge \
+    --name $container_name \
     --env ACCEPT_EULA=Y \
-    --env MSSQL_SA_PASSWORD=<YourSqlPasswordHere> \
-    --volume /Users/<YourSystemUsername>/docker/sqledge/staging:/var/opt/mssql/staging \
-    --volume /Users/<YourSystemUsername>/docker/sqledge/data:/var/opt/mssql/data \
-    --volume /Users/<YourSystemUsername>/docker/sqledge/log:/var/opt/mssql/log \
-    --volume /Users/<YourSystemUsername>/docker/sqledge/secrets:/var/opt/mssql/secrets \
+    --env MSSQL_SA_PASSWORD=$initial_password \
+    --volume $HOME/Containers/sqledge/staging:/var/opt/mssql/staging \
+    --volume $HOME/Containers/sqledge/data:/var/opt/mssql/data \
+    --volume $HOME/Containers/sqledge/log:/var/opt/mssql/log \
+    --volume $HOME/Containers/sqledge/secrets:/var/opt/mssql/secrets \
     --publish 1433:1433 \
     --restart always \
     mcr.microsoft.com/azure-sql-edge
