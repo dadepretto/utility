4deca688aeabde2b16786009d0dd8130a07a8242
Davide De Pretto <42669027+dadepretto@users.noreply.github.com>
2022-04-12 08:40:04 +0200

    Improve default version

--- sql-server-scripts/diagnose-sessions.sql
+++ sql-server-scripts/diagnose-sessions.sql
@@ -1,24 +1,22 @@
 with
     [Sessions] as (
         select
-            sysutcdatetime()              as [timestamp],
-            db_name([S].[database_id])    as [database_name],
-            [S].[session_id]              as [session_id],
-            [T].[text]                    as [sql_text],
-            [S].[login_name]              as [login_name],
-            [R].[wait_time]               as [wait_time],
-            [R].[wait_type]               as [wait_type],
-            [S].[cpu_time]                as [cpu_time],
-            [S].[reads]                   as [reads],
-            [S].[writes]                  as [writes],
-            [S].[memory_usage]            as [memory_usage],
-            [S].[status]                  as [status],
-            [R].[open_transaction_count]  as [open_transaction_count],
-            [S].[host_name]               as [host_name],
-            [S].[program_name]            as [program_name],
-            [S].[last_request_start_time] as [last_request_start_time],
-            [S].[login_time]              as [login_time],
-            [P].[query_plan]              as [plan]
+            [S].[session_id],
+            [T].[text],
+            [S].[login_name],
+            [R].[wait_time],
+            [R].[wait_type],
+            [S].[cpu_time],
+            [S].[reads],
+            [S].[writes],
+            [S].[memory_usage],
+            [S].[status],
+            [R].[open_transaction_count],
+            [S].[host_name],
+            [S].[program_name],
+            [S].[last_request_start_time],
+            [S].[login_time],
+            [P].[query_plan]
         from [sys].[dm_exec_sessions] as [S]
             left join [sys].[dm_exec_connections] as [C]
                 on [S].[session_id] = [C].[session_id]
@@ -27,28 +25,30 @@ with
             outer apply [sys].[dm_exec_sql_text](
                 [C].[most_recent_sql_handle]
             ) as [T]
-            outer apply [sys].[dm_exec_query_plan]([R].[plan_handle]) as [P]
-        where [S].[is_user_process] = 1
+            outer apply [sys].[dm_exec_query_plan](
+                [R].[plan_handle]
+            ) as [P]
+        where [S].[database_id] = db_id()
             and [S].[session_id] <> @@spid
+            and [S].[is_user_process] = 1
     )
 select
-    [timestamp],
-    [database_name],
-    [session_id],
-    [sql_text],
-    [login_name],
-    [wait_time],
-    [wait_type],
-    [cpu_time],
-    [reads],
-    [writes],
-    [memory_usage],
-    [status],
-    [open_transaction_count],
-    [host_name],
-    [program_name],
-    [last_request_start_time],
-    [login_time],
-    [plan]
+    sysutcdatetime()            as [timestamp],
+    [session_id]                as [session_id],
+    [text]                      as [sql_text],
+    [login_name]                as [login_name],
+    [wait_time]                 as [wait_time],
+    [wait_type]                 as [wait_type],
+    [cpu_time]                  as [cpu_time],
+    [reads]                     as [reads],
+    [writes]                    as [writes],
+    [memory_usage]              as [memory_usage],
+    [status]                    as [status],
+    [open_transaction_count]    as [open_transaction_count],
+    [host_name]                 as [host_name],
+    [program_name]              as [program_name],
+    [last_request_start_time]   as [last_request_start_time],
+    [login_time]                as [login_time],
+    [query_plan]                as [plan]
 from [Sessions]
-order by [database_name];
+order by [login_name];
